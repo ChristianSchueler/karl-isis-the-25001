f713af8c086717c8e272fe05b59435655be5a34f
Christian Schüler <christian.schueler@wetouch.at>
2023-12-10 17:11:49 +0100

    gpiotest

---
+++ gpiotest.js
@@ -0,0 +1,16 @@
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(7, 'out');
+const button = new Gpio(5, 'in', 'both');
+
+button.watch((err, value) => {
+    if (err) {
+      throw err;
+    }
+  
+    led.writeSync(value);
+  });
+  
+  process.on('SIGINT', _ => {
+    led.unexport();
+    button.unexport();
+  });
